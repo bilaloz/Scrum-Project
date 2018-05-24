3a5902755d34df78b9f3d0992c43d77de233755f
Bilal ÖZ <152805013@ogr.cbu.edu.tr>
2018-05-24 14:27:48 +0300

    Complete

---
+++ WindowsFormsApplication83/Story.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication83
+{
+   public class Story
+    {
+       
+        
+        public string StoryBasligi { get; set; }
+        public string StoryAciklamasi { get; set; }
+        
+    }
+}
